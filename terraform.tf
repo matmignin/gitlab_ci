d5f59d78e9519b93730b13828d3831b87db3faea
mathough <mathough@gmail.com>
2019-06-12 13:21:32 -0400

    add load balancer`

--- terraform.tf
+++ terraform.tf
@@ -23,6 +23,38 @@ resource "aws_instance" "test-instance" {
   }
 }
 
+
+#           ____________________  Load Balancer stuff _____________
+
+resource "aws_elb" "test-lb" {
+  name    = "test-lb"
+  availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  instances       = ["${aws_instance.ants-instance.*.id}"]
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    interval = 30
+    target = "HTTP:80/"
+  }
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 4
+}
+
+
+
 output "instance_ips" {
   value = ["${aws_instance.test-instance.*.public_ip}"]
 }
