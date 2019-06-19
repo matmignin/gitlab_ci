2be2eaabba311be6301edf034a8fc0389f81365e
mathough <mathough@gmail.com>
2019-06-18 20:45:52 -0400

    update to terraform .12 and fix syntax.

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,33 +1,30 @@
 # TerraForm Backend
 terraform {
   backend "s3" {
-    bucket = "iac-bucket"
-    key = "terraform/terraform.tfstate"
-    region = "us-east-2"
+    bucket = "iac-bucket2"
+    key    = "terraform/terraform.tfstate"
   }
 }
-
 
 # AWS Provider
 provider "aws" {
   region = "us-east-2"
 }
 
-# Import State "global" From Remote S3 Bucket
-data “terraform_remote_state” “global” {
-  backend = “s3”
-  config {
-    region = "us-east-2"
-    bucket = "iac-bucket"
+# Import State global From Remote S3 Bucket
+data "terraform_remote_state" "network" {
+  backend = "s3"
+  config = {
+    bucket = "iac-bucket2"
     key = "terraform/terraform.tfstate"
   }
 }
 
 resource "aws_instance" "iac-instance" {
-  count = 4
+  count         = 2
   ami           = "ami-0f65671a86f061fcd"
   instance_type = "t2.micro"
-  key_name = "iac-test"
+  key_name      = "iac-test"
 
   provisioner "remote-exec" {
     inline = [
@@ -36,11 +33,12 @@ resource "aws_instance" "iac-instance" {
       "sudo mkdir new_folder",
     ]
     connection {
-        type = "ssh"
-        user = "ubuntu"
+      host = coalesce(self.public_ip, self.private_ip)
+      type = "ssh"
+      user = "ubuntu"
     }
   }
-  tags {
+  tags = {
     Name = "iac-${count.index}"
   }
 }
@@ -48,16 +46,16 @@ resource "aws_instance" "iac-instance" {
 #   __________________  Load Balancer stuff _____________
 
 resource "aws_elb" "iac-lb" {
-  name    = "iac-lb"
+  name               = "iac-lb"
   availability_zones = ["us-east-2a", "us-east-2b", "us-east-2c"]
-  instances       = ["${aws_instance.iac-instance.*.id}"]
+  instances          = aws_instance.iac-instance.*.id
 
   health_check {
-    healthy_threshold = 2
+    healthy_threshold   = 2
     unhealthy_threshold = 2
-    timeout = 3
-    interval = 30
-    target = "HTTP:80/"
+    timeout             = 3
+    interval            = 30
+    target              = "HTTP:80/"
   }
 
   listener {
@@ -67,7 +65,6 @@ resource "aws_elb" "iac-lb" {
     lb_protocol       = "http"
   }
 
-
   cross_zone_load_balancing   = true
   idle_timeout                = 400
   connection_draining         = true
@@ -75,10 +72,10 @@ resource "aws_elb" "iac-lb" {
 }
 
 output "instance_ips" {
-  value = ["${aws_instance.iac-instance.*.public_ip}"]
+  value = [aws_instance.iac-instance.*.public_ip]
 }
 
 output "address" {
-  value = "${aws_elb.iac-lb.dns_name}"
+  value = aws_elb.iac-lb.dns_name
 }
 
