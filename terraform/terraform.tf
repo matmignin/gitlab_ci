b5d7acf2b8f5f094cb8d8599d516d8b47ebfdce5
mathough <mathough@gmail.com>
2019-06-24 14:13:47 -0400

    fixed yaml file apply block

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,5 +1,5 @@
-variable "bucket_name" {
-  description = "the name to give the bucket"jj<S-F5>
+#variable "bucket_name" {
+  #description = "the name to give the bucket"jj<S-F5>
 
 
 # TerraForm Backend
@@ -76,6 +76,6 @@ output "address" {
   value = aws_elb.iac-lb.dns_name
 }
 
-output "bucket" {
-  value = s3-remote-state-bucket.bucket_id
-}
+#output "bucket" {
+  #value = s3-remote-state-bucket.bucket_id
+#}
